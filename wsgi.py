fac38c3001dde3174c5362c0bcaed0010743130b
skwisgaarskwigelf <sabina.sfn@gmail.com>
2019-04-20 13:54:39 +0300

    uwsgi flask

--- wsgi.py
+++ wsgi.py
@@ -2,3 +2,6 @@ from crossposting import app
 
 if __name__ == "__main__":
     app.run()
+#def application(env, start_response):
+#    start_response('200 OK', [('Content-Type', 'text/html')])
+#    return ["Hello!"]
