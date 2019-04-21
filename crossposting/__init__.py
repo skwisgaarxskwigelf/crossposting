9f2b9e2ef90c65c66fc7076448b87411970b63ef
skwisgaarskwigelf <sabina.sfn@gmail.com>
2019-04-22 02:22:02 +0300

    minor changes

--- crossposting/__init__.py
+++ crossposting/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
+#    app.config.from_object(os.environ['APP_SETTINGS'])
+#    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
     db.init_app(app)
 
     from .index import index as index_blueprint
