0af05d07f3fcffb3bb5e9a9b2fc2bc93f2a8e02a
skwisgaarskwigelf <sabina.sfn@gmail.com>
2019-04-22 02:02:18 +0300

    minor changes

--- crossposting/__init__.py
+++ crossposting/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
+    db.init_app(app)
 
     from .index import index as index_blueprint
     app.register_blueprint(index_blueprint, url_prefix='/')
