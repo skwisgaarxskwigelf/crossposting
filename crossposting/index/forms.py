5041e707bca5401a2e35986e7334a9b031da948a
fffgf <sabina.sfn@gmail.com>
2019-05-05 23:51:28 +0300

    boolean field fix

--- crossposting/index/forms.py
+++ crossposting/index/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, validators, ValidationError
+from wtforms import StringField, BooleanField, SubmitField, validators, ValidationError
 
 
 class ChannelsForm(FlaskForm):
